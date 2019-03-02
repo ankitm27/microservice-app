13e56fbb733c1b56882e87ed76da7c7a8faf9d36
ankitm27 <anki.malhotra00@gmail.com>
2019-03-02 11:48:09 +0530

    grapgql schema added

--- packages/gateway/server.js
+++ packages/gateway/server.js
@@ -1,11 +1,11 @@
 const server = require('express')();
 const bodyParser = require('body-parser');
+const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
 
 server
-.use(bodyParser.json())
-.get('/',(_,res) => {
-    res.send("check");
-})
-.listen(3000,()=>{
-    console.log("check123");
-})
+  .use(bodyParser.json())
+  .use('/graphql',graphqlExpress({ schema }))
+  .use('/gq',graphiqlExpress({ endpointURL : '/graphql' }))
+  .listen(3000,()=>{
+      console.log("check123");
+  })
